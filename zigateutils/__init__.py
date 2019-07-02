19eb5e6e4e5278627bfb99a9223d33aa1377405f
jsl-1 <mail@jsl1.fr>
2019-07-02 10:43:10 +0200

    Get Zigate entity component

--- zigateutils/__init__.py
+++ zigateutils/__init__.py
@@ -6,10 +6,11 @@ import logging
 import voluptuous as vol
 from homeassistant.const import ATTR_ENTITY_ID
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity_component import DATA_INSTANCES
 
 # from homeassistant.helpers.entity_component import EntityComponent
 # from .frontend.views.nework import (
-    # ZigateNetworkView,
+#    ZigateNetworkView,
 # )
 
 DOMAIN = 'zigateutils'
@@ -25,12 +26,16 @@ LEAVE_AND_REJOIN_SCHEMA = vol.Schema({
     vol.Optional(ADDR): cv.string,
     vol.Optional(IEEE): cv.string,
     vol.Optional(ATTR_ENTITY_ID): cv.entity_id,
-	vol.Optional('rejoin'): cv.boolean,
-	vol.Optional('remove_children'): cv.boolean,
+    vol.Optional('rejoin'): cv.boolean,
+    vol.Optional('remove_children'): cv.boolean,
 })
+
 
 def setup(hass, config):
     myzigate = hass.data[ZIGATE_DOMAIN]
+    zigate_entitycomponent = hass.data.get(
+        DATA_INSTANCES, {}).get(ZIGATE_DOMAIN)
+
     # component = EntityComponent(_LOGGER, DOMAIN, hass,
     #                             group_name=GROUP_NAME_ALL_ZIGATEUTILS)
 
@@ -42,7 +47,7 @@ def setup(hass, config):
         ieee = service.data.get(IEEE)
         addr = service.data.get(ADDR)
         if entity_id:
-            entity = component.get_entity(entity_id)
+            entity = zigate_entitycomponent.get_entity(entity_id)
             if entity:
                 addr = entity._device.addr
         elif ieee:
@@ -51,12 +56,27 @@ def setup(hass, config):
                 addr = device.addr
         return addr
 
+    def _get_ieee_from_service_request(service):
+        entity_id = service.data.get(ATTR_ENTITY_ID)
+        ieee = service.data.get(IEEE)
+        addr = service.data.get(ADDR)
+        if entity_id:
+            entity = zigate_entitycomponent.get_entity(entity_id)
+            if entity:
+                ieee = entity._device.ieee
+        elif addr:
+            device = myzigate.get_device_from_addr(addr)
+            if device:
+                ieee = device.ieee
+        return ieee
+
     def leave_and_rejoin(service):
-        addr = service.data.get('addr')
-        ieee = service.data.get('ieee', None)
+        addr = _get_addr_from_service_request(service)
+        ieee = _get_ieee_from_service_request(service)
         if addr:
-          myzigate.permit_join()
-          myzigate.leave_request(addr, ieee=ieee, rejoin=True, remove_children=False)
+            myzigate.permit_join()
+            myzigate.leave_request(addr, ieee=ieee, rejoin=True,
+                                   remove_children=False)
 
     hass.services.register(DOMAIN, 'leave_and_rejoin', leave_and_rejoin,
                            schema=LEAVE_AND_REJOIN_SCHEMA)
